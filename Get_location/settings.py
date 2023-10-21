c0f512a93b35302dae69576f3ed2f5bbe877f4df
midou18 <ahalamo18@gmail.com>
2023-10-21 14:03:14 +0100

    add hosts

--- Get_location/settings.py
+++ Get_location/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k-3ti==*7ohn$8jsf4jd+_^v1fs^i#1kopgv=(yn&5ppskziag
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
